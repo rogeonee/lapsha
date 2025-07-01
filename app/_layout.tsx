2a820f1e8f6bd1b12ed15970254d9edbfd0135f8
Egor B. <egor.bezriadin.01@gmail.com>
2025-06-30 20:18:57 -0700

    chore: rnr setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,58 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React, { useRef, useState } from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
 import '../global.css';
 
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export { ErrorBoundary } from 'expo-router';
+
 export default function RootLayout() {
-  return <Stack />;
+  const hasMounted = useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      document.documentElement.classList.add('bg-background');
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      <Stack />
+    </ThemeProvider>
+  );
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === 'web' && typeof window === 'undefined'
+    ? React.useEffect
+    : React.useLayoutEffect;
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Button } from '~/components/ui/button';
+import { Text } from '~/components/ui/text';
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-purple-100">
-      <Text className="text-3xl font-bold">Welcome to Lapsha!</Text>
+      <Text className="text-3xl font-bold mb-10">Welcome to Lapsha!</Text>
+      <Button
+        variant="outline"
+        size="lg"
+        onPress={() => alert('Button pressed!')}
+      >
+        <Text>Press me</Text>
+      </Button>
     </View>
   );
 }
