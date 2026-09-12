64f76c7373545f15a85f426c7c1fc3c88155026c
Egor B. <egor.bezriadin.01@gmail.com>
2026-09-11 23:54:52 -0600

    fix/stack-large-text-birthday-date

--- src/components/person/birthday-date-row.ios.tsx
+++ src/components/person/birthday-date-row.ios.tsx
@@ -1,5 +1,5 @@
 import { DatePicker, Host } from '@expo/ui/swift-ui';
-import { View } from 'react-native';
+import { View, useWindowDimensions } from 'react-native';
 import { Text } from '~/components/ui/text';
 
 type BirthdayDateRowProps = {
@@ -18,16 +18,32 @@ export default function BirthdayDateRow({
   date,
   onChange,
 }: BirthdayDateRowProps) {
+  const { fontScale } = useWindowDimensions();
+  const usesExpandedRow = fontScale > 1;
+
+  const picker = (
+    <Host matchContents>
+      <DatePicker
+        selection={date}
+        displayedComponents={['date']}
+        onDateChange={onChange}
+      />
+    </Host>
+  );
+
+  if (usesExpandedRow) {
+    return (
+      <View className="items-start gap-2 border-t border-black/5 px-4 py-3">
+        <Text className="text-base">Date</Text>
+        <View className="self-end">{picker}</View>
+      </View>
+    );
+  }
+
   return (
     <View className="flex-row items-center justify-between border-t border-black/5 px-4 py-2">
       <Text className="text-base">Date</Text>
-      <Host matchContents>
-        <DatePicker
-          selection={date}
-          displayedComponents={['date']}
-          onDateChange={onChange}
-        />
-      </Host>
+      {picker}
     </View>
   );
 }
